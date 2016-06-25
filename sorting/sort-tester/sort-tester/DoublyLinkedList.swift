b35436eaedada27502d294cbd19b364d4602a13b
Marcus Grant <marcusfg@gmail.com>
2016-06-25 18:22:55 -0400

    Add structures playground

---
+++ sorting/sort-tester/sort-tester/DoublyLinkedList.swift
@@ -0,0 +1,9 @@
+//
+//  DoublyLinkedList.swift
+//  sort-tester
+//
+//  Created by Marcus Grant on 6/25/16.
+//  Copyright © 2016 Marcus Grant. All rights reserved.
+//
+
+import Foundation
---
+++ structures/LinkedList.playground/Contents.swift
@@ -0,0 +1,65 @@
+//
+//  DoublyLinkedList.swift
+//  sort-tester
+//
+//  Created by Marcus Grant on 6/25/16.
+//  Copyright © 2016 Marcus Grant. All rights reserved.
+//  Using code from RayWenderlich.com http://bit.ly/28Vnhfn
+//
+
+import Foundation
+
+public class LinkedList<T> {
+  public typealias Node = LinkedListNode<T>
+  private var head: Node?
+  private var tail: Node?
+
+  public var isEmpty: Bool {
+    return head == nil
+  }
+
+  public var first: Node? {
+    return head
+  }
+
+  public var last: Node? {
+    if var node = head {
+      while case let next? = node.next {
+        node = next
+      }
+      return node
+    } else { return nil }
+  }
+
+  public func append(value: T) {
+    let newNode = Node(value: value)
+    if let lastNode = last {
+      newNode.previous = lastNode
+      lastNode.next = newNode
+    } else {
+      head = newNode
+    }
+  }
+}
+
+
+public class LinkedListNode<T> {
+  var value: T
+  var next: LinkedListNode?
+  var previous: LinkedListNode?
+
+  public init(value: T) {
+    self.value = value
+  }
+}
+
+//Testing
+/////////////////////////////////////////////////////////
+
+var list = LinkedList<String>()
+list.isEmpty              // true
+list.first                // nil
+list.append("Hello!")
+list.isEmpty              // false
+list.first!.value         // "Hello!"
+list.last!.value          // "Hello!" - first & last are the same
